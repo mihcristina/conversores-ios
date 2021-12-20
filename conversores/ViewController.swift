46af88c132f5b995d7b033dbb0dc82a65b0e7512
Michelli <contato@michellicristina.com.br>
2021-12-20 13:35:54 -0300

    chore: modifica função showNext

--- conversores/ViewController.swift
+++ conversores/ViewController.swift
@@ -14,18 +14,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var btUnit2: UIButton!
     @IBOutlet weak var lbResult: UILabel!
     @IBOutlet weak var lsResultUnit: UILabel!
-    
+    @IBOutlet weak var lbUnit: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
-    @IBAction func showNext(_ sender: UIButton) {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
-    @IBAction func convert(_ sender: UIButton) {
+    
+    @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text! {
+            case "Temperatura":
+                lbUnit.text = "Peso"
+                btUnit1.setTitle("Kilograma", for: .normal)
+                btUnit2.setTitle("Libra", for: .normal)
+                
+            case "Peso":
+                lbUnit.text = "Moeda"
+                btUnit1.setTitle("Real", for: .normal)
+                btUnit2.setTitle("Dólar", for: .normal)
+                
+            case "Moeda":
+                lbUnit.text = "Distância"
+                btUnit1.setTitle("Metro", for: .normal)
+                btUnit2.setTitle("Kilômetro", for: .normal)
+                
+            default:
+                lbUnit.text = "Temperatura"
+                btUnit1.setTitle("Celsius", for: .normal)
+                btUnit2.setTitle("Farenhint", for: .normal)
+                
+            }
+        convert(nil)
+    }
+    
+    @IBAction func convert(_ sender: UIButton?) {
+        
     }
 }
 
