709fdb6670cbb4faa0a37ca36d6d994bb2c4714c
Michelli <contato@michellicristina.com.br>
2021-12-20 14:25:53 -0300

    chore: adiciona funções de operadores e modifica frase da label

--- conversores/ViewController.swift
+++ conversores/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var lsResultUnit: UILabel!
     @IBOutlet weak var lbUnit: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -53,7 +54,68 @@ class ViewController: UIViewController {
     }
     
     @IBAction func convert(_ sender: UIButton?) {
-        
+        if let sender = sender {
+            if sender == btUnit1 {
+                btUnit2.alpha = 0.5
+            } else {
+                btUnit1.alpha = 0.5
+            }
+            sender.alpha = 1.0
+        }
+        switch lbUnit.text! {
+            case "Temperatura":
+                calcTemperature()
+            case "Peso":
+                calcWeight()
+            case "Moeda":
+                calcCurrency()
+            default:
+                calcDistance()
+            }
+    }
+    
+    func calcTemperature() {
+        guard let temperate = Double(tfValue.text!) else { return }
+        if btUnit1.alpha == 1.0 {
+            lsResultUnit.text = "Fareinheit"
+            lbResult.text = String(temperate * 1.8 + 32.0)
+        } else {
+            lsResultUnit.text = "Celsius"
+            lbResult.text = String((temperate - 1.8) + 32.0)
+        }
+    }
+    
+    func calcWeight() {
+        guard let distance = Double(tfValue.text!) else { return }
+        if btUnit1.alpha == 1.0 {
+            lsResultUnit.text = "Kilômetro"
+            lbResult.text = String(distance / 1000.0)
+        } else {
+            lsResultUnit.text = "Metro"
+            lbResult.text = String(distance * 1000.0)
+        }
+    }
+    
+    func calcCurrency() {
+        guard let weight = Double(tfValue.text!) else { return }
+        if btUnit1.alpha == 1.0 {
+            lsResultUnit.text = "Libra"
+            lbResult.text = String(weight / 2.2046)
+        } else {
+            lsResultUnit.text = "Kilôgrama"
+            lbResult.text = String(weight * 2.2046)
+        }
+    }
+    
+    func calcDistance() {
+        guard let distance = Double(tfValue.text!) else { return }
+        if btUnit1.alpha == 1.0 {
+            lsResultUnit.text = "Kilômetro"
+            lbResult.text = String(distance / 1000.0)
+        } else {
+            lsResultUnit.text = "Metro"
+            lbResult.text = String(distance * 1000.0)
+        }
     }
 }
 
